445864f2d4c1259ff926d5a1b44e499722981e9c
Serhii Tarasenko <stdev33@gmail.com>
2024-03-02 12:59:05 +0200

    correct styles

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import css from './MainLayout.module.css';
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className={css.layoutContainer}>
       <Header />
-      <Outlet />
+      <div className={css.contentContainer}>
+        <Outlet />
+      </div>
     </div>
   );
 };
